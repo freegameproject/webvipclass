91871da20ad4f1e32e734611608dd60e8e267717
freegameproject <freegameproject@sina.com>
2015-12-23 18:37:08 +0800

    class 1

---
+++ nodejs/class_1/app.js
@@ -0,0 +1,8 @@
+var http     = require('http');
+var path     = require('path');
+var express  = require('express');
+var app = express();
+app.use(express.static(path.join(__dirname, '/public')));
+http.createServer(app).listen(3000, function(){
+    console.log("Express server listening on port " + 3000);
+});
